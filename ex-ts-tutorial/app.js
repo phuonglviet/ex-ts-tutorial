accd401f41caa9620b554be759741db158b0febf
Phuong.Le <le_viet_phuong@sackle.co.jp>
2019-07-10 19:01:54 +0900

    add model

--- ex-ts-tutorial/app.js
+++ ex-ts-tutorial/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var mongooseConn = require('./mongooseConn');
+
 var app = express();
 
 // view engine setup
--- ex-ts-tutorial/src/models/author.ts
+++ ex-ts-tutorial/src/models/author.ts
@@ -1,22 +1,64 @@
-import { Document, Schema, Model } from "mongoose";
-import { Moment } from "moment";
+import { Schema, Document, Model, model } from 'mongoose';
+import * as moment from 'moment'
 
 export interface IAuthor extends Document {
-    first_name: {type: String, required: true, max: 100};
-    family_name: {type: String, required: true, max: 100};
-    date_of_birth: {type: Date};
-    date_of_death: {type: Date};
+    first_name: String;
+    family_name: String;
+    date_of_birth: Date;
+    date_of_death: Date;
 }
 
-export class AuthorModel {
+var AuthorSchema = new Schema(
+    {
+      first_name: {type: String, required: true, max: 100},
+      family_name: {type: String, required: true, max: 100},
+      date_of_birth: {type: Date},
+      date_of_death: {type: Date},
+    }
+);
 
-    /**
-   * Constructor
-   *
-   * @constructor
-   */
-  constructor() {
-    
-  }
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
 
-}
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  var lifetime_string='';
+  if (this.date_of_birth) {
+      lifetime_string=moment(this.date_of_birth).format('MMMM Do, YYYY');
+      }
+  lifetime_string+=' - ';
+  if (this.date_of_death) {
+      lifetime_string+=moment(this.date_of_death).format('MMMM Do, YYYY');
+      }
+  return lifetime_string
+});
+
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+AuthorSchema
+.virtual('date_of_birth_yyyy_mm_dd')
+.get(function () {
+  return moment(this.date_of_birth).format('YYYY-MM-DD');
+});
+
+AuthorSchema
+.virtual('date_of_death_yyyy_mm_dd')
+.get(function () {
+  return moment(this.date_of_death).format('YYYY-MM-DD');
+});
+
+// Export the model and return your IUser interface
+// export default model<IAuthor>('Author', AuthorSchema);
+export const Author: Model<IAuthor> = model<IAuthor>("Author", AuthorSchema);
